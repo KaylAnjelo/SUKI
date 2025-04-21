72c577ab57c5aefcd04d91d930406856bf7f6cdb
KaylAnjelo <kalmario828@gmail.com>
2025-04-21 14:26:59 +0800

    Try p4

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  database: process.env.MYSQLDATABASE,
+  port : process.env.MYSQLPORT
 });
 
 // Static file directory
