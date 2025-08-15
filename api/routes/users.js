c1568992f9759fd2398ba5357ad07dadc0149835
emmotivated <emmotivated21@gmail.com>
2025-08-15 17:15:23 +0800

    Additional Changes

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,4 +10,16 @@ router.get('/UserManagement', (req, res) => {
     res.render('users/UserManagement');
 });
 
+router.get('/Customer', (req, res) => {
+    res.render('users/Customer');
+});
+
+router.get('/Store', (req, res) => {
+    res.render('users/Store');
+});
+
+router.get('/Vendor', (req, res) => {
+    res.render('users/Vendor');
+});
+
 export default router;
